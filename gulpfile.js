0991c13087c709f06a3d824d69ea88d44abd0fba
Ben Rothman <bensrothman@gmail.com>
2015-09-01 08:18:25 -0500

    update linting. playing around with es6

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
 
-gulp.task('stylesheets', function() {
-    gulp.src('main.scss')
-        .pipe(plugins.sass())
-        .pipe(gulp.dest('dist'));
+gulp.task('stylesheets', function stylesheets() {
+  gulp.src('main.scss')
+      .pipe(plugins.sass())
+      .pipe(gulp.dest('dist'));
 });
 
-gulp.task('javascript-lint', function() {
-    return gulp.src('*.js')
-        .pipe(plugins.eslint())
-        .pipe(plugins.eslint.format());
+gulp.task('javascript-lint', function javascriptLint() {
+  return gulp.src('*.js')
+             .pipe(plugins.eslint())
+             .pipe(plugins.eslint.format());
 });
 
-gulp.task('javascript', ['javascript-lint'], function() {
-    gulp.src('main.js')
-        .pipe(plugins.babel())
-        .pipe(gulp.dest('dist'));
+gulp.task('javascript', ['javascript-lint'], function javascript() {
+  gulp.src('main.js')
+      .pipe(plugins.babel())
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function watch() {
+  gulp.watch('*.js', ['javascript']);
 });
 
 gulp.task('default', ['stylesheets', 'javascript']);
--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-var x = [1, 2, 3, 4, 5];
+(function thing() {
+  const x = [1, 2, 3, 4, 5];
 
-console.log(x.map(a => a * 2 + 1));
+  console.log(x.map(a => a * 2 + 1));
 
-if (x.length) {
+  if (x.length) {
     let y = 5;
-}
+    console.log(y);
+    y++;
+  }
 
-console.log(y);
+  console.log(y);
+})();
